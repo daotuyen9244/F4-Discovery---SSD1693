ce5a1d4f6260b743fc3a519a4ade64a0bd2a331d
TuyenDao <daotuyen9244@gmail.com>
2018-08-23 19:55:12 +0700

    Add FSMC interface

---
+++ TFT7INCH_FSMC/MDK-ARM/RTE/_TFT7INCH_FSMC/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TFT7INCH_FSMC' 
+ * Target:  'TFT7INCH_FSMC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
