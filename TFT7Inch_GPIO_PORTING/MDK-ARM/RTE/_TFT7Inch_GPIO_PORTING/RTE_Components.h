aa2c830a1544fc50028cfc0e3892a5d62ec4d3dc
TuyenDao <daotuyen9244@gmail.com>
2018-08-23 19:22:26 +0700

    Add F4 with SSD1963 using GPIO

---
+++ TFT7Inch_GPIO_PORTING/MDK-ARM/RTE/_TFT7Inch_GPIO_PORTING/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TFT7Inch_GPIO_PORTING' 
+ * Target:  'TFT7Inch_GPIO_PORTING' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
